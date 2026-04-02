4d9f16d4ffa426d5cd523d667e5422698e53ef5b
Juuso <juuso.sav@gmail.com>
2026-04-02 14:52:21 +0300

    Add: mobile nav closing when navigating

---
+++ CodingPortfolioWebsite/wwwroot/js/nav.js
@@ -0,0 +1,14 @@
+﻿window.navHelper = {
+    closeMenu: function () {
+        const toggler = document.getElementById('navbarNav');
+        if (toggler) {
+            toggler.checked = false;
+        }
+
+        const navCollapse = document.getElementById('navbarNav');
+        if (navCollapse && window.bootstrap) {
+            const collapse = bootstrap.Collapse.getInstance(navCollapse);
+            if (collapse) collapse.hide();
+        }
+    }
+}
